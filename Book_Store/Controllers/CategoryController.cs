bb0c77a651c2c1377a329ee5b7a5cec5ba59451f
Mohammed Waleed <mwaleedx1@gmail.com>
2024-09-04 19:17:22 +0300

    Changing Style of Project

--- Book_Store/Controllers/CategoryController.cs
+++ Book_Store/Controllers/CategoryController.cs
@@ -1,12 +1,20 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Book_Store.Data;
+using Book_Store.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Book_Store.Controllers
 {
     public class CategoryController : Controller
     {
+        private readonly ApplicationDbContext _context;
+        public CategoryController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
         public IActionResult Index()
         {
-            return View();
+            List<Category> CategoryList = _context.Categories.ToList();
+            return View(CategoryList);
         }
     }
 }
