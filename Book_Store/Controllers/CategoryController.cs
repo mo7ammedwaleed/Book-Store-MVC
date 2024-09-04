7d75bf689a00acadb1d7374d3f88fcaee300ae02
Mohammed Waleed <mwaleedx1@gmail.com>
2024-09-04 18:06:21 +0300

    Adding Category Section to NavBar

---
+++ Book_Store/Controllers/CategoryController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Book_Store.Controllers
+{
+    public class CategoryController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
