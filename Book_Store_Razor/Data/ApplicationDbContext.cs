802fc8c57e531842fd7a5b6fc435eb723d18b188
Mohammed Waleed <mwaleedx1@gmail.com>
2025-02-20 19:14:27 +0200

    Adding ApplicationDbContext

---
+++ Book_Store_Razor/Data/ApplicationDbContext.cs
@@ -0,0 +1,25 @@
+﻿using Book_Store.Razor.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Book_Store.Razor.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+            
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Category>()
+                .HasData(
+                        new Category { Id = 1 , Name = "Action" , DisplayOrder = 1},
+                        new Category { Id = 2 , Name = "Scifi" , DisplayOrder = 2},
+                        new Category { Id = 3 , Name = "History" , DisplayOrder = 3}
+                );
+
+            base.OnModelCreating(modelBuilder);
+        }
+        public DbSet<Category> Categories { get; set; }
+    }
+}
