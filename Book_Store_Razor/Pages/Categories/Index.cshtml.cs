bb08058db90239fd3ec7a0fdd0b02f9cc4c42aac
Mohammed Waleed <mwaleedx1@gmail.com>
2025-02-21 23:27:34 +0200

    Adding Category index page in Razor project

---
+++ Book_Store_Razor/Pages/Categories/Index.cshtml.cs
@@ -0,0 +1,22 @@
+using Book_Store.Razor.Data;
+using Book_Store.Razor.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Book_Store_Razor.Pages.Categories
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ApplicationDbContext _db;
+
+        public List<Category> CategoryList { get; set; }
+        public IndexModel(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        public void OnGet()
+        {
+            CategoryList = _db.Categories.ToList();
+        }
+    }
+}
