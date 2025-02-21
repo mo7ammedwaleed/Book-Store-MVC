a77cca20e2cb21cd22c8c5c213d84887b379bc12
Mohammed Waleed <mwaleedx1@gmail.com>
2025-02-22 00:31:54 +0200

    Adding Edit Page

---
+++ Book_Store_Razor/Pages/Categories/Edit.cshtml.cs
@@ -0,0 +1,37 @@
+using Book_Store.Razor.Data;
+using Book_Store.Razor.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+
+namespace Book_Store_Razor.Pages.Categories
+{
+    public class EditModel : PageModel
+    {
+        private readonly ApplicationDbContext _db;
+        [BindProperty]
+        public Category? Category { get; set; }
+        public EditModel(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        public void OnGet(int? id)
+        {
+            if (id != null && id != 0)
+            {
+                Category = _db.Categories.Find(id);
+            }
+        }
+        public IActionResult OnPost()
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Categories.Update(Category);
+                _db.SaveChanges();
+                TempData["success"] = "Category Updated Successfuly";
+                return RedirectToPage("Index");
+            }
+            return Page();
+        }
+    }
+}
