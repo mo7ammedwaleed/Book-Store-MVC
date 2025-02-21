f1f65a9e013fd5ace59d84751220a9c0fe266e77
Mohammed Waleed <mwaleedx1@gmail.com>
2025-02-22 00:32:26 +0200

    Adding Delete Page

---
+++ Book_Store_Razor/Pages/Categories/Delete.cshtml.cs
@@ -0,0 +1,38 @@
+using Book_Store.Razor.Data;
+using Book_Store.Razor.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+
+namespace Book_Store_Razor.Pages.Categories
+{
+    public class DeleteModel : PageModel
+    {
+        private readonly ApplicationDbContext _db;
+        [BindProperty]
+        public Category? Category { get; set; }
+        public DeleteModel(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        public void OnGet(int? id)
+        {
+            if (id != null && id != 0)
+            {
+                Category = _db.Categories.Find(id);
+            }
+        }
+        public IActionResult OnPost()
+        {
+            Category? obj = _db.Categories.Find(Category.Id);
+            if (obj == null)
+            {
+                return NotFound();
+            }
+            _db.Categories.Remove(obj);
+            _db.SaveChanges();
+            TempData["success"] = "Category Deleted Successfuly";
+            return RedirectToPage("Index");
+        }
+    }
+}
