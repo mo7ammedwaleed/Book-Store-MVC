370fcca90d5feb347f0760dba5d7437ee70d42b5
Mohammed Waleed <mwaleedx1@gmail.com>
2025-02-24 19:15:02 +0200

    Implement ICategoryRepository in CategoryRepository class

---
+++ BookStore.DataAccess/Repository/CategoryRepository.cs
@@ -0,0 +1,24 @@
+﻿using System.Linq.Expressions;
+using BookStore.DataAccess.Data;
+using BookStore.DataAccess.Repository.IRepository;
+using BookStore.Models;
+
+namespace BookStore.DataAccess.Repository
+{
+    public class CategoryRepository : Repository<Category>,ICategoryRepository
+    {
+        private readonly ApplicationDbContext _db;
+        public CategoryRepository(ApplicationDbContext db) : base(db)
+        {
+            _db = db;
+        }
+        public void Update(Category obj)
+        {
+            _db.Update(obj);
+        }
+        public void Save()
+        {
+            _db.SaveChanges();
+        }
+    }
+}
