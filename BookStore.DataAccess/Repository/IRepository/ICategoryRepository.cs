5688935daf9eeb145c22c4a3fe3c789b3c665664
Mohammed Waleed <mwaleedx1@gmail.com>
2025-02-24 19:08:16 +0200

    Adding ICategoryRepository Interface

---
+++ BookStore.DataAccess/Repository/IRepository/ICategoryRepository.cs
@@ -0,0 +1,10 @@
+﻿using BookStore.Models;
+
+namespace BookStore.DataAccess.Repository.IRepository
+{
+    interface ICategoryRepository : IRepository<Category>
+    {
+        void Update(Category obj);
+        void Save();
+    }
+}
