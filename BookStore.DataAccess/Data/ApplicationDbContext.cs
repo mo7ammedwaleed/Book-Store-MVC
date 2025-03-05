913fa8a24d5157cb889a6fe995a85d918eb67713
Mohammed Waleed <mwaleedx1@gmail.com>
2025-03-05 20:16:40 +0200

    Extend Identity User

--- BookStore.DataAccess/Data/ApplicationDbContext.cs
+++ BookStore.DataAccess/Data/ApplicationDbContext.cs
@@ -113,5 +113,6 @@ namespace BookStore.DataAccess.Data
         }
         public DbSet<Category> Categories { get; set; }
         public DbSet<Product> Products { get; set; }
+        public DbSet<ApplicationUser> ApplicationUsers { get; set; }
     }
 }
