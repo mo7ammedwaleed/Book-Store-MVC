c4a000bfd8d4960b4eee35e1c06d798ce9fd13bf
Mohammed Waleed <mwaleedx1@gmail.com>
2025-03-07 21:17:28 +0200

    Finishing register page and update database

--- BookStore.Models/ApplicationUser.cs
+++ BookStore.Models/ApplicationUser.cs
@@ -6,10 +6,12 @@ namespace BookStore.Models
     public class ApplicationUser : IdentityUser
     {
         [Required]
+        [Display(Name = "Full Name")]
         public string Name { get; set; }
         public string? StreetAddress { get; set; }
         public string? City { get; set; }
         public string? State { get; set; }
         public string? PostalCode { get; set; }
+        public string? PhoneNumber { get; set; }
     }
 }
