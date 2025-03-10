19ccf670617713c885a86edf515352cb57b16d5c
Mohammed Waleed <mwaleedx1@gmail.com>
2025-03-10 20:49:01 +0200

    Add ApplicationUser Repository

---
+++ BookStore.DataAccess/Repository/ApplicationUserRepository.cs
@@ -0,0 +1,16 @@
+﻿using System.Linq.Expressions;
+using BookStore.DataAccess.Data;
+using BookStore.DataAccess.Repository.IRepository;
+using BookStore.Models;
+
+namespace BookStore.DataAccess.Repository
+{
+    public class ApplicationUserRepository : Repository<ApplicationUser>,IApplicationUserRepository
+    {
+        private readonly ApplicationDbContext _db;
+        public ApplicationUserRepository(ApplicationDbContext db) : base(db)
+        {
+            _db = db;
+        }
+    }
+}
---
+++ BookStore.DataAccess/Repository/IRepository/IApplicationUserRepository.cs
@@ -0,0 +1,8 @@
+﻿using BookStore.Models;
+
+namespace BookStore.DataAccess.Repository.IRepository
+{
+    public interface IApplicationUserRepository : IRepository<ApplicationUser>
+    {
+    }
+}
