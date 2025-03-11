6c0fd767b193d796a2085ed4a8f6f57cbda41d81
Mohammed Waleed <mwaleedx1@gmail.com>
2025-03-11 03:47:14 +0200

    Get Shopping Cart

--- BookStore.DataAccess/Repository/Repository.cs
+++ BookStore.DataAccess/Repository/Repository.cs
@@ -19,7 +19,7 @@ namespace BookStore.DataAccess.Repository
         {
             dbSet.Add(entity);
         }
-        public T Get(Expression<Func<T, bool>> filter, string? includeProperties = null, bool tracked = false)
+        public T Get(Expression<Func<T, bool>>? filter, string? includeProperties = null, bool tracked = false)
         {
             IQueryable<T> query;
             if (tracked)
@@ -43,9 +43,13 @@ namespace BookStore.DataAccess.Repository
             }
             return query.FirstOrDefault();
         }
-        public IEnumerable<T> GetAll(string? includeProperties = null)
+        public IEnumerable<T> GetAll(Expression<Func<T, bool>> filter, string? includeProperties = null)
         {
             IQueryable<T> query = dbSet;
+            if (filter != null)
+            {
+                query = query.Where(filter);
+            }
 
             if (!string.IsNullOrEmpty(includeProperties))
             {
