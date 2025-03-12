c867a55f82b764416a7e7a3b93f0f0c3918d738c
Mohammed Waleed <mwaleedx1@gmail.com>
2025-03-12 20:27:32 +0200

    Create Order Header and Details Model

--- BookStore.DataAccess/Data/ApplicationDbContext.cs
+++ BookStore.DataAccess/Data/ApplicationDbContext.cs
@@ -11,6 +11,14 @@ namespace BookStore.DataAccess.Data
         {
             
         }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Company> Companies { get; set; }
+        public DbSet<ShoppingCart> ShoppingCarts { get; set; }
+        public DbSet<OrderHeader> OrderHeaders { get; set; }
+        public DbSet<OrderDetail> OrderDetails { get; set; }
+        public DbSet<ApplicationUser> ApplicationUsers { get; set; }
+
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
 
@@ -144,10 +152,5 @@ namespace BookStore.DataAccess.Data
                         }
                 );
         }
-        public DbSet<Category> Categories { get; set; }
-        public DbSet<Product> Products { get; set; }
-        public DbSet<Company> Companies { get; set; }
-        public DbSet<ShoppingCart> ShoppingCarts { get; set; }
-        public DbSet<ApplicationUser> ApplicationUsers { get; set; }
     }
 }
