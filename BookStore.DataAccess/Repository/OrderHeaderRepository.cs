d5762c85e875dda2fa83530975debea52b6698b3
Mohammed Waleed <mwaleedx1@gmail.com>
2025-03-15 19:39:29 +0200

    Add Helper Methods in Order Header Repository

--- BookStore.DataAccess/Repository/OrderHeaderRepository.cs
+++ BookStore.DataAccess/Repository/OrderHeaderRepository.cs
@@ -16,5 +16,32 @@ namespace BookStore.DataAccess.Repository
         {
             _db.OrderHeaders.Update(obj);
         }
+
+        public void UpdateStatus(int id, string orderStatus, string? paymentStatus = null)
+        {
+            var orderFromDB = _db.OrderHeaders.FirstOrDefault(x => x.Id == id);
+            if (orderFromDB != null)
+            {
+                orderFromDB.OrderStatus = orderStatus;
+                if (!string.IsNullOrEmpty(paymentStatus))
+                {
+                    orderFromDB.PaymentStatus = paymentStatus;
+                }
+            }
+        }
+
+        public void UpdateStripePaymentID(int id, string sessionId, string paymentIntentId)
+        {
+            var orderFromDB = _db.OrderHeaders.FirstOrDefault(x => x.Id == id);
+            if (!string.IsNullOrEmpty(sessionId))
+            {
+                orderFromDB.SessionId = sessionId;
+            }
+            if (!string.IsNullOrEmpty(paymentIntentId))
+            {
+                orderFromDB.PaymentIntentId = paymentIntentId;
+                orderFromDB.PaymentDate = DateTime.Now;
+            }
+        }
     }
 }
