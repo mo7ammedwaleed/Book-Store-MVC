284695b591dc045b8dde3a4b460d9ede97d4b17c
Mohammed Waleed <mwaleedx1@gmail.com>
2025-03-19 13:03:23 +0200

    Order List UI

--- Book_Store/Areas/Admin/Controllers/OrderController.cs
+++ Book_Store/Areas/Admin/Controllers/OrderController.cs
@@ -5,6 +5,7 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace BookStore.Areas.Admin.Controllers
 {
+    [Area("Admin")]
     public class OrderController : Controller
     {
         private readonly IUnitOfWork _unitOfWork;
