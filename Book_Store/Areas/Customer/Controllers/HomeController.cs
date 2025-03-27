abcd95c684acfb370f83ec66d3f7acb2f16e46e6
Mohammed Waleed <mwaleedx1@gmail.com>
2025-03-27 07:04:32 +0200

    Session in .NET Core And ShoppingCart Item Count

--- Book_Store/Areas/Customer/Controllers/HomeController.cs
+++ Book_Store/Areas/Customer/Controllers/HomeController.cs
@@ -1,6 +1,8 @@
 using BookStore.DataAccess.Repository.IRepository;
 using BookStore.Models;
+using BookStore.Utility;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
 using System.Security.Claims;
@@ -51,15 +53,17 @@ namespace BookStore.Areas.Customer.Controllers
                 // shoppingCart exists in database
                 cartFromDb.Count += shoppingCart.Count;
                 _unitOfWork.ShoppingCart.Update(cartFromDb);
+                _unitOfWork.Save();
                 TempData["success"] = "Cart Updated successfully";
             }
             else
             {
                 // add card record
                 _unitOfWork.ShoppingCart.Add(shoppingCart);
+                _unitOfWork.Save();
+                HttpContext.Session.SetInt32(SD.SessionCart, _unitOfWork.ShoppingCart.GetAll(e => e.ApplicationUserId == userId).Count());
                 TempData["success"] = "Item added to cart successfully";
             }
-            _unitOfWork.Save();
 
             return RedirectToAction(nameof(Index));
         }
--- Book_Store/Program.cs
+++ Book_Store/Program.cs
@@ -26,6 +26,14 @@ builder.Services.ConfigureApplicationCookie(options =>
     options.AccessDeniedPath = $"/Identity/Account/AccessDenied";
 });
 
+builder.Services.AddDistributedMemoryCache();
+builder.Services.AddSession(options =>
+{
+    options.IdleTimeout = TimeSpan.FromMinutes(100);
+    options.Cookie.HttpOnly = true;
+    options.Cookie.IsEssential = true;
+});
+
 builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
 builder.Services.AddScoped<IEmailSender, EmailSender>();
 
@@ -48,6 +56,7 @@ app.UseRouting();
 
 app.UseAuthentication();
 app.UseAuthorization();
+app.UseSession();
 
 app.MapRazorPages();
 app.MapControllerRoute(
