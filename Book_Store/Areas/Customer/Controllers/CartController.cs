5997ab54585445e79a5aa87aafec00c4d16c07cb
Mohammed Waleed <mwaleedx1@gmail.com>
2025-03-28 12:36:37 +0200

    Session Bug

--- Book_Store/Areas/Customer/Controllers/CartController.cs
+++ Book_Store/Areas/Customer/Controllers/CartController.cs
@@ -217,6 +217,7 @@ namespace BookStore.Areas.Customer.Controllers
                     _unitOfWork.OrderHeader.UpdateStatus(id,SD.StatusApproved,SD.PaymentStatusApproved);
                     _unitOfWork.Save();
                 }
+                HttpContext.Session.Clear();
             }
 
             List<ShoppingCart> shoppingCarts = _unitOfWork.ShoppingCart
