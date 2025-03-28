f94fb0356df0e70a5d48e75dd7a401b920275189
Mohammed Waleed <mwaleedx1@gmail.com>
2025-03-28 16:13:12 +0200

    Display User List

---
+++ Book_Store/Areas/Admin/Controllers/UserController.cs
@@ -0,0 +1,50 @@
+﻿using BookStore.DataAccess.Repository.IRepository;
+using BookStore.Models.ViewModels;
+using BookStore.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using System.Data;
+using Microsoft.AspNetCore.Authorization;
+using BookStore.Utility;
+using BookStore.DataAccess.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace BookStore.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    [Authorize(Roles = SD.Role_Admin)]
+    public class UserController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+        public UserController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        public IActionResult Index()
+        {
+            List<ApplicationUser> objUserList = _db.ApplicationUsers.Include(u => u.Company).ToList();
+
+            return View(objUserList);
+        }
+
+
+        #region API CALLS
+
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            List<ApplicationUser> objUserList = _db.ApplicationUsers.Include(u => u.Company).ToList();
+            return Json(new { data = objUserList });
+        }
+
+        [HttpDelete]
+        public IActionResult Delete(int? id)
+        {
+
+
+            return Json(new { success = true, message = "User Deleted Successfully" });
+        }
+
+        #endregion
+    }
+}
